b61e2a84da092e064eb5cff1490d3c2d8836c983
n0rb3r7 <n0rb3r7@outlook.com>
2020-10-24 18:12:08 -0300

    Update en index
    Todavía seguimos probando esto.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,54 @@
 <template>
 <q-page class="flex">
 <template>
+              <q-carousel
+        style="width:100%"
+        v-model="slide"
+        transition-prev="scale"
+        transition-next="scale"
+        swipeable
+        animated
+        control-color="black"
+        navigation
+        padding
+        arrows
+        height="500px"
+        class="text-black shadow-1 rounded-borders"
+      >
+        <q-carousel-slide name="style" color="black" class="column no-wrap flex-center">
+          <q-icon name="style" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="tv" class="column no-wrap flex-center">
+          <q-icon name="live_tv" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="layers" class="column no-wrap flex-center">
+          <q-icon name="layers" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+        <q-carousel-slide name="map" class="column no-wrap flex-center">
+          <q-icon name="terrain" size="56px" />
+          <div class="q-mt-md text-center">
+            {{ lorem }}
+          </div>
+        </q-carousel-slide>
+      </q-carousel>
     <q-parallax
       src="https://www.placecage.com/1920/1080"
     >
-      <h1 class="text-white">Basic</h1>
+      <h1 class="text-white">I don't know</h1>
+    </q-parallax>
+        <q-parallax
+      src="https://www.placecage.com/c/1920/1080"
+    >
+      <h1 class="text-white">I don't know 2</h1>
     </q-parallax>
 </template>
 </q-page>
@@ -12,6 +56,12 @@
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data () {
+    return {
+      slide: 'style',
+      lorem: 'I dun like carousels, but idk. Se ven raros los controles dots, con píxeles, wth? Los parallax son feos tmb. Todo plano if possible.'
+    }
+  }
 }
 </script>
