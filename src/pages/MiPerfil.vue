9d9e527250de2b54aefe2ef784c3124035f6a1d7
Laura <51368962+xlxm01@users.noreply.github.com>
2020-10-31 21:47:10 -0300

    Funcion getUser en Mi Perfil
    Se agrego la funcion get User(), trae los datos del usuario que esta logueado. Guardando en el encavezado del axios el token.

--- src/pages/MiPerfil.vue
+++ src/pages/MiPerfil.vue
@@ -2,13 +2,15 @@
 <q-page class="flex flex-center">
     <h1>Bienvenido</h1>
     <div>
-        <q-btn class="btn-xs" @click="logout">Cerrar Sesion</q-btn>
+       <q-btn class="btn-xs" @click="logout">Cerrar Sesion</q-btn>
+      <div>{{user.data}}</div>
+      <div v-if="">{{creator.data}}</div>
     </div>
 </q-page>
 </template>
 
 <script>
-// import Axios from 'axios'
+import * as axios from 'axios'
 export default {
   name: 'MiPerfil',
   data () {
@@ -34,20 +36,27 @@ export default {
       sessionStorage.removeItem('apiToken')
       this.user = []
       this.$router.push(this.$route.query.redirect || '/')
-    }
-    // Busco mis datos de usuario enviando mi token
-    /* getUser: async function () {
-      try {
-        const data = await fetch('http://localhost:8000/api/users/me?apiToken=' + sessionStorage.getItem('apiToken'))
-        const response = await data.json()
-        // console.log(response.data.id)
-        this.user = response.data
-      } catch (error) {
-        console.error(error)
-      }
     },
+    // Busco mis datos de usuario enviando mi token
+    getUser () {
+      axios.defaults.headers = {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + sessionStorage.getItem('apiToken')
+      }
+      axios.get('http://localhost:8000/api/users/me', {
+        token: sessionStorage.getItem('apiToken')
+      })
+        .then((response) => {
+          console.log(response.data)
+          this.user = response.data
+          // Buscar mis datos si soy creador
+        })
+        .catch(err => {
+          console.log(err.response)
+        })
+    }
     // Busco mis datos si soy creator enviando mi id de usuario
-    getCreator: async function () {
+    /* getCreator() {
       try {
         const data = await fetch('http://localhost:8000/api/creator/idUser=' + )
         const response = await data.json()
