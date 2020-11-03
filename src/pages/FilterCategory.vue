bce5cbe68333ae6b149c45bc0fa72d069048b01c
Laura <51368962+xlxm01@users.noreply.github.com>
2020-11-03 00:04:47 -0300

    Modificación Filtro por Categoria
    Se muestra los creadores de la categoria elegia en una lista

--- src/pages/FilterCategory.vue
+++ src/pages/FilterCategory.vue
@@ -1,13 +1,13 @@
 <template>
 <q-page class="flex flex-center">
-    <h1>Creadores x Categorias {{creatorsXCat[0]}}</h1>
+    <h1>Creadores x Categorias</h1>
     <q-list id="creadoresXCategoria">
       <q-item
         v-for="creator in creatorsXCat"
         :key="creator.id"
       >
         <q-item-section>
-          <q-item-label>Creador: ---- {{creator.banner}}</q-item-label>
+          <q-item-label>Creador: ---- {{creator}}</q-item-label>
         </q-item-section>
       </q-item>
     </q-list>
@@ -19,7 +19,7 @@ import * as axios from 'axios'
 
 export default {
   name: 'FilterCategory',
-  data: function () {
+  data () {
     return {
       creatorsXCat: []
     }
@@ -33,14 +33,21 @@ export default {
   methods: {
     getCreators_Categoria: async function () {
       var idCategory = this.$route.params.idCategory
-      alert(idCategory)
-      axios.get('http://localhost:8000/catCreators/' + idCategory)
-        .then(function (response) {
+      // alert(idCategory)
+      axios.get('http://localhost:8000//catCreators/' + idCategory)
+        .then((response) => {
           this.creatorsXCat = response.data
-          console.log(response.data)
+          // console.log(this.creatorsXCat)
+          // alert(response.data[0].banner)
         })
-        .catch(function (error) {
+        .catch((error) => {
           console.log(error)
+          // alert(error)
+          this.$q.notify({
+            message: 'Lo sentimos, vuelva a intentarlo más tarde.',
+            color: 'warning'
+          })
+          this.$q.notify('')
         })
     }
   }
