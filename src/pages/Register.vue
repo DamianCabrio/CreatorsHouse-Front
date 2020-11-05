cadb11b0e13ead435b2ddd3d3d5e2ddfb3a026f9
n0rb3r7 <n0rb3r7@outlook.com>
2020-11-05 17:22:13 -0300

    Update del registro y cambio color $negative en el tema.
    Se agrega validación confirmación del password y al menos 8 caracteres de longitud.
    No funciona la función isValidPassword con el regex. Idk why.
    El color $negative se cambia del tema para diferenciarlo del primary cuando valida inputs.

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,69 +1,103 @@
 <template>
 
-<q-page class="flex flex-center">
-  <div class="q-pa-md fit row wrap justify-start items-start content-start">
-    <q-form
-      @submit="onSubmit"
-      @reset="onReset"
-      class="q-gutter-md col-md-4 col-12 self-center offset-md-4"
-    >
-      <div class="text-h3 q-mb-none">Registrarse</div>
-      <q-input
-        filled
-        v-model="username"
-        label="Username *"
-        hint="Tu nombre en Creator house"
-        lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Ingresa un nickname']"
-      />
+  <q-page class="flex flex-center">
+    <div class="q-pa-md fit row wrap justify-start items-start content-start">
+      <q-form
+        @submit="onSubmit"
+        @reset="onReset"
+        class="q-gutter-md col-md-4 col-12 self-center offset-md-4"
+      >
+        <div class="text-h3 q-mb-none text-weight-thin">Registrarse</div>
+        <q-input
+          filled
+          v-model="username"
+          label="Username *"
+          hint="Tu nombre en Creator house"
+          lazy-rules
+          :rules="[ val => val && val.length > 0 || 'Ingresa un nickname']"
+        />
 
-      <q-input
-        filled
-        type="email"
-        v-model="email"
-        label="Email *"
-        lazy-rules
-        :rules="[
+        <q-input
+          filled
+          type="email"
+          v-model="email"
+          hint="Ejemplo: usuario@mail.com"
+          label="Email *"
+          lazy-rules
+          :rules="[
           val => val !== null && val !== '' || 'Ingresa tu email'
         ]"
-      />
-      <q-input v-model="password" filled :type="isPwd ? 'password' : 'text'" hint="" label="Contraseña *">
-        <template v-slot:append>
-          <q-icon
-            :name="isPwd ? 'visibility_off' : 'visibility'"
-            class="cursor-pointer"
-            @click="isPwd = !isPwd"
-          />
-        </template>
-      </q-input>
-      <q-input v-model="password2" filled :type="isPwd ? 'password' : 'text'" hint="Repetir Contraseña">
-        <template v-slot:append>
-          <q-icon
-            :name="isPwd ? 'visibility_off' : 'visibility'"
-            class="cursor-pointer"
-            @click="isPwd = !isPwd"
-          />
-        </template>
-      </q-input>
-      <q-input
-        filled
-        type="date"
-        v-model="birthDate"
-        label=""
-        hint="Fecha de nacimiento"
-        lazy-rules
-        :rules="[
+        />
+        <q-input
+          v-model="password"
+          filled
+          :type="isPwd ? 'password' : 'text'"
+          hint=""
+          label="Contraseña *"
+          lazy-rules
+          :rules="[
+          val => val !== null && val !== '' || 'Contraseña requerida',
+          val => val && val.length >= 8 || 'Ingresa una contraseña con al menos 8 carcateres',
+          isValidPassword
+        ]"
+        >
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+          </template>
+        </q-input>
+        <q-input
+          v-model="password2"
+          filled
+          :type="isPwd ? 'password' : 'text'"
+          label="Confirmar Contraseña *"
+          error-message="Contraseña no coincide."
+          :error="!confirmPassword"
+        >
+          <template v-slot:append>
+            <q-icon
+              :name="isPwd ? 'visibility_off' : 'visibility'"
+              class="cursor-pointer"
+              @click="isPwd = !isPwd"
+            />
+          </template>
+        </q-input>
+        <q-input
+          filled
+          type="date"
+          v-model="birthDate"
+          label=""
+          hint="Fecha de nacimiento"
+          lazy-rules
+          :rules="[
           val => val !== null && val !== '' || 'Ingrese su fecha de nacimiento'
         ]"
-      />
-      <q-toggle v-model="accept" label="Yo acepto los términos y condiciones" />
-      <div>
-        <q-btn label="Enviar" type="submit" color="primary" @click="register"/>
-        <q-btn label="Borrar" type="reset" color="primary" flat class="q-ml-sm" />
-      </div>
-    </q-form>
-  </div>
-</q-page>
+        />
+        <q-toggle
+          v-model="accept"
+          label="Yo acepto los términos y condiciones"
+        />
+        <div>
+          <q-btn
+            label="Enviar"
+            type="submit"
+            color="primary"
+            @click="register"
+          />
+          <q-btn
+            label="Borrar"
+            type="reset"
+            color="primary"
+            flat
+            class="q-ml-sm"
+          />
+        </div>
+      </q-form>
+    </div>
+  </q-page>
 </template>
 
 <script>
@@ -83,7 +117,17 @@ export default {
       accept: false
     }
   },
-
+  computed: {
+    confirmPassword () {
+      return this.password === this.password2
+    },
+    isValidPassword (val) {
+      // no funca
+      // >80 https://digitalfortress.tech/tricks/top-15-commonly-used-regex/
+      const passwordPattern = /(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,}$/
+      return passwordPattern.test(val) || 'La constraseña debe tener al menos una letra mayúscula, un número y un caracter especial.'
+    }
+  },
   methods: {
     register () {
       axios.post('http://localhost:8000/users', {
