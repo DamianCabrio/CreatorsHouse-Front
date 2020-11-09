0549b05d8d31c8cf47de412cf7262e89e89718c5
n0rb3r7 <n0rb3r7@outlook.com>
2020-11-09 17:32:12 -0300

    Actualización index y creación componente ShowThreeCreatorsHome

---
+++ src/components/ShowThreeCreatorsHome.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="row">
+    <div
+      v-for='user in users'
+      :key="user.id"
+      class="col-12 col-md-4"
+    >
+      <div class="q-pa-lg">
+        <q-card class="my-card">
+          <q-item>
+            <q-item-section avatar>
+              <q-btn
+                flat
+                round
+                color="secondary"
+                icon="share"
+              />
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label>@{{user.username}}</q-item-label>
+              <q-item-label caption>{{user.name}}</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-img
+            :ratio="1.8/1"
+            v-bind:src="`http://localhost:8000/img/${user.avatar}`"
+          >
+          </q-img>
+        </q-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ShowThreeCreatorsHome',
+  data () {
+    return {
+      users: []
+    }
+  },
+  mounted () {
+    this.getCreators()
+  },
+  methods: {
+    getCreators: async function () {
+      try {
+        const data = await fetch('http://localhost:8000/userCreatorsHome')
+        const response = await data.json()
+        // console.log(response.data.id)
+        this.users = response.data
+      } catch (error) {
+        console.error(error)
+      }
+    }
+  }
+}
+</script>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,14 +7,17 @@
             class="q-pl-md q-pr-md q-pb-md"
             style="text-align:left"
           >
-            <div class="text-white text-bold text-h3 q-mt-xl q-pt-xl q-pb-lg">Recibí donaciones y ofrecé contenido premium a tus fans.</div>
-            <div class="text-white text-bold text-h4 q-pr-lg q-pb-lg">Registrate para empezar ahora mismo.</div>
-            <q-btn
-              color="white"
-              text-color="black"
-              label="Registrarse"
-              to="/Register"
-            />
+            <div class="text-white text-bold text-h3 index-title q-mt-xl q-pt-xl q-pb-lg">Recibí donaciones y ofrecé contenido premium a tus fans.</div>
+            <div class="text-white text-bold text-h4 q-pr-lg q-pb-xl">Registrate para empezar ahora mismo.</div>
+            <div class="q-pt-xl">
+              <q-btn
+                color="white"
+                text-color="black"
+                label="Registrarse"
+                to="/Register"
+                size="lg"
+              />
+            </div>
           </div>
         </div>
         <div class="col-md-5 col-12">
@@ -26,119 +29,66 @@
           </div>
         </div>
       </div>
-      <div class="row justify-center">
+      <div class="row justify-center bg-three-creators q-pt-xl q-pb-xl">
         <div class="col-12 col-md-8">
-          <div class="text-h4 q-pa-lg text-bold text-center">Conocé nuevos creadores</div>
-          <div class="row">
-            <div
-              v-for='user in users'
-              class="col-12 col-md-4"
-              :key="user.id"
-            >
-              <div class="q-pa-lg">
-                <q-card>
-                  <q-img  :ratio="1.5/1" v-bind:src="`http://localhost:8000/img/${user.avatar}`">
-                    <div class="absolute-bottom">
-                      <div class="text-h6">@{{user.username}}</div>
-                      <div class="text-subtitle2">{{user.name}}</div>
-                    </div>
-                  </q-img>
-
-                  <q-card-actions class="">
-                    <q-btn
-                      flat
-                      round
-                      color="red"
-                      icon="favorite"
-                    />
-                    <q-btn
-                      flat
-                      round
-                      color="primary"
-                      icon="share"
-                    />
-                  </q-card-actions>
-                </q-card>
-              </div>
-            </div>
-          </div>
+          <ShowThreeCreatorsHome />
         </div>
       </div>
     </div>
     <template>
-      <q-carousel
-        style="width:100%"
-        v-model="slide"
-        transition-prev="scale"
-        transition-next="scale"
-        swipeable
-        animated
-        control-color="black"
-        navigation
-        padding
-        arrows
-        height="500px"
-        class="text-black shadow-1 rounded-borders"
-      >
-        <q-carousel-slide
-          name="style"
-          color="black"
-          class="column no-wrap flex-center"
-        >
-          <q-icon
-            name="style"
-            size="56px"
-          />
-          <div class="q-mt-md text-center">
-            {{ lorem }}
+      <div class="container">
+        <div class="row justify-center q-pt-xl q-pb-xl">
+          <div class="col-12 col-md-10">
+            <q-card
+              flat
+              bordered
+            >
+              <q-card-section
+                style="min-height:20vh;background-color:#EDEFF3"
+                class="no-padding"
+              >
+                <div class="row">
+                  <div
+                    class="col-12 col-md-6 flex"
+                    style="align-items: center;"
+                  >
+                    <div class="q-pa-lg">
+                      <div class="text-h4 text-weight-light">También podés apoyar a tus creadores favoritos y alentarlos para que sigan haciendo lo que aman.</div>
+                    </div>
+                  </div>
+                  <div class="col-12 col-md-6">
+                    <q-img
+                      src="https://images.pexels.com/photos/374068/pexels-photo-374068.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+                      :ratio="16/9"
+                    />
+                  </div>
+                </div>
+              </q-card-section>
+              <q-card-section class="bg-secondary text-white q-pa-lg flex">
+                <div class="text-h5 text-weight-light q-pr-md">
+                  Registrate para apoyar a tus creadores.
+                </div>
+                <q-btn
+                  color="white"
+                  text-color="black"
+                  label="Registrarse"
+                  to="/Register"
+                />
+              </q-card-section>
+            </q-card>
           </div>
-        </q-carousel-slide>
-        <q-carousel-slide
-          name="tv"
-          class="column no-wrap flex-center"
-        >
-          <q-icon
-            name="live_tv"
-            size="56px"
-          />
-          <div class="q-mt-md text-center">
-            {{ lorem }}
-          </div>
-        </q-carousel-slide>
-        <q-carousel-slide
-          name="layers"
-          class="column no-wrap flex-center"
-        >
-          <q-icon
-            name="layers"
-            size="56px"
-          />
-          <div class="q-mt-md text-center">
-            {{ lorem }}
-          </div>
-        </q-carousel-slide>
-        <q-carousel-slide
-          name="map"
-          class="column no-wrap flex-center"
-        >
-          <q-icon
-            name="terrain"
-            size="56px"
-          />
-          <div class="q-mt-md text-center">
-            {{ lorem }}
-          </div>
-        </q-carousel-slide>
-      </q-carousel>
+        </div>
+      </div>
     </template>
   </q-page>
 </template>
 <script>
+import ShowThreeCreatorsHome from 'components/ShowThreeCreatorsHome.vue'
 export default {
   name: 'PageIndex',
+  components: { ShowThreeCreatorsHome },
   data () {
     return {
-      user: [],
       users: [],
       info: null,
       slide: 'style',
@@ -162,3 +112,27 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+.index-title {
+  span {
+    background-image: linear-gradient(
+      transparent calc(65% - 5px),
+      darken($accent, 15%) 5px
+    );
+    background-size: 0;
+    background-repeat: no-repeat;
+    display: inline;
+    transition: 0.5s ease;
+  }
+
+  &:hover {
+    span {
+      background-size: 100%;
+    }
+  }
+}
+.bg-three-creators {
+  background-image: linear-gradient($primary 50%, #ffffff 0%);
+}
+</style>
